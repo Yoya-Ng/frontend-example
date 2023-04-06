2cccf5b0f2985b390e4e6cf5b3fcc4679ff8edf9
unknown <pt06@firstins.com.tw>
2023-04-06 10:45:05 +0800

    message

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
+import { configureStore } from "@reduxjs/toolkit";
+import todoReducer from "./redux/todoReducer";
+import Counter from "./redux/Counter";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = configureStore({
+  reducer: {
+    Counter,
+    todoReducer,
+  },
+});
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
